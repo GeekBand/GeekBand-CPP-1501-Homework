68dd1ac4ae5ec5a382c3117647ea0197a0662d23
Zhiqiang Zhang <zhangzhiqiang235@sina.cn>
2015-08-09 09:24:12 +0800

    c++ homework

---
+++ G2015010570/Rectangle.cpp
@@ -0,0 +1,47 @@
+#include "Rectangle.h"
+
+Rectangle::Rectangle(int width, int height, int x, int y, int no):Shape(no) {
+	this->width = width;
+	this->height = height;
+	this->leftUp = new Point(x, y);
+}
+
+Rectangle::~Rectangle() {
+	delete leftUp;
+}
+
+Rectangle::Rectangle(const Rectangle& other):Shape(other.no) {
+	this->width = other.width;
+	this->height = other.height;
+	this->leftUp = new Point(other.getX(), other.getY());
+}
+
+void Rectangle::setHeight(int height) {
+	this->height = height;
+}
+
+int Rectangle::getHeight() const {
+	return this->height;
+}
+
+void Rectangle::setWidth(int width) {
+	this->width = width;
+}
+
+int Rectangle::getWidth() const {
+	return this->width;
+}
+
+void Rectangle::setX(int x) {
+	this->leftUp->setX(x);
+}
+int Rectangle::getX() const {
+	return this->leftUp->getX();
+}
+void Rectangle::setY(int y) {
+	this->leftUp->setY(y);
+}
+int Rectangle::getY() const {
+	return this->leftUp->getY();
+}
+
