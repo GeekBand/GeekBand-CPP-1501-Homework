79910c0992f5200d56711628524296f61eb5c1c0
zhangzhiqiang235 <zhangzhiqiang235@sina.cn>
2015-08-10 11:53:36 +0800

    adjust output

--- G2015010570/main.cpp
+++ G2015010570/main.cpp
@@ -6,30 +6,30 @@ int main(int argc, char** argv) {
 	cout << "no=" << s->getNo() << endl;
 
 	Rectangle* r = dynamic_cast<Rectangle*>(s);
-	cout<<"address of s="<<s<<endl;
-	cout<<"address of r="<<r<<endl;
-	cout << "width=" << r->getWidth() << endl;
-	cout << "weight=" << r->getHeight() << endl;
-	cout << "x=" << r->getX() << endl;
-	cout << "y=" << r->getY() << endl;
+	cout << "address of s=" << s << endl;
+	cout << "address of r=" << r << endl;
+	cout << "width of r=" << r->getWidth() << endl;
+	cout << "height or r=" << r->getHeight() << endl;
+	cout << "x or r=" << r->getX() << endl;
+	cout << "y or r=" << r->getY() << endl;
 
-	cout<<endl;
+	cout << endl;
 
 	Rectangle r2 = *r;
-	cout<<"address of r2="<<&r2<<endl;
-	cout << "r2 width=" << r2.getWidth() << endl;
-	cout << "r2 weight=" << r2.getHeight() << endl;
-	cout << "r2 x=" << r2.getX() << endl;
-	cout << "r2 y=" << r2.getY() << endl;
+	cout << "address of r2=" << &r2 << endl;
+	cout << "width of r2=" << r2.getWidth() << endl;
+	cout << "height of r2=" << r2.getHeight() << endl;
+	cout << "x of r2=" << r2.getX() << endl;
+	cout << "y of r2=" << r2.getY() << endl;
 
-	cout<<endl;
+	cout << endl;
 
 	Rectangle* r3 = new Rectangle(*r);
-	cout<<"address of r3="<<r3<<endl;
-	cout << "r3 width=" << r3->getWidth() << endl;
-	cout << "r3 weight=" << r3->getHeight() << endl;
-	cout << "r3 x=" << r3->getX() << endl;
-	cout << "r3 y=" << r3->getY() << endl;
+	cout << "address of r3=" << r3 << endl;
+	cout << "width of r3=" << r3->getWidth() << endl;
+	cout << "weight of r3=" << r3->getHeight() << endl;
+	cout << "x of r3=" << r3->getX() << endl;
+	cout << "y of r3=" << r3->getY() << endl;
 	delete s;
 	delete r3;
 }
