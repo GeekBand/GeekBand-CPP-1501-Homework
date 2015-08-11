8988228ae0b1cc9986997851b049190d90f6e7f6
Yue Niu <elaineniuyue@gmail.com>
2015-08-10 21:45:53 -0400

    Update rectangle.h

--- G2015010571/rectangle.h
+++ G2015010571/rectangle.h
@@ -28,12 +28,13 @@ public:
 
 Rectangle::Rectangle(int width, int height, int x, int y): width(width), height(height), leftUp(new Point(x, y)){ }
 
-Rectangle::Rectangle(const Rectangle& other): width(other.width), height(other.height), leftUp(new Point(*other.leftUp)){ }
+Rectangle::Rectangle(const Rectangle& other): Shape(other), width(other.width), height(other.height), leftUp(new Point(*other.leftUp)){ }
 
 Rectangle& Rectangle::operator=(const Rectangle& other){
 	if(this == &other){
 		return *this;
 	}
+	Shape::operator=(other);
 	delete leftUp;
 	width = other.width;
 	height = other.height;
