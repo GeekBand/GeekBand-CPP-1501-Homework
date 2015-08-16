13222e8caa0e659cc6ab2d5fc0e79d7b36d8acb2
Jay <q5707802@163.com>
2015-08-16 18:38:09 +0800

    q5707802

---
+++ G2015010441/rectangle.cpp
@@ -0,0 +1,52 @@
+#include "rectangle.h"
+
+
+Rectangle::Rectangle(int _width, int _height, int _x, int _y)
+	: Shape(), width(_width), height(_height), leftUp(new Point(_x, _y))
+{
+
+}
+
+Rectangle::Rectangle(const Rectangle& other)
+	: Shape(), width(other.width), height(other.height)
+{
+	if(other.leftUp != NULL)
+	{
+		leftUp = new Point(other.leftUp->getX(), other.leftUp->getY());
+	}
+	else
+		leftUp = NULL;
+}
+
+Rectangle& Rectangle::operator=(const Rectangle& other)
+{
+	if(this == &other)
+		return *this;
+
+	width = other.width;
+	height = other.height;
+
+	if(this->leftUp != other.leftUp)
+	{
+		if(other.leftUp != NULL)
+		{
+			if(leftUp != NULL)
+				delete leftUp;
+
+			leftUp = new Point(other.leftUp->getX(), other.leftUp->getY());
+		}
+		else
+		{
+			delete leftUp;
+			leftUp = NULL;
+		}
+	}
+
+    return *this;
+}
+
+Rectangle::~Rectangle()
+{
+	if(leftUp != NULL)
+		delete leftUp;
+}
