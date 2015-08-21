ae4c2e09c7b16ee0c0f359ef13754f627a961043
gzlkylin <gzlkylin@gmail.com>
2015-08-21 16:33:27 +0800

    Add Main Function
    Add Main Function

--- G2015010304/C++Ex2/Rectangle/Rectangle/main.cpp
+++ G2015010304/C++Ex2/Rectangle/Rectangle/main.cpp
@@ -3,12 +3,12 @@
 #include "Rectangle.h"
 using namespace std;
 #define RAND10() (rd() % 10 + 1)
+#define SHAPE_NUM 20
 
-int main()
+void shapeInit(Shape** shape, int size)
 {
-	Shape* shape[20];
 	std::random_device rd;
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < size; i++) {
 		if (i < 10) {
 			shape[i] = new Rectangle(RAND10(), RAND10(), RAND10(), RAND10());
 		}
@@ -17,22 +17,52 @@ int main()
 		}
 		shape[i]->setNo(i);
 	}
-	
-	int count = 0;
+}
+
+Shape** selectArea50(Shape** shape, int size, int& count)
+{
+	Shape** shapeArea50;
+	int num = 0;
 	int delNum = 0;
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < size; i++) {
 		if (shape[i]->getArea() < 50) {
 			delNum++;
 			delete shape[i];
 		}
-		else{
-			shape[count] = shape[i];
-			count++;
+		else {
+			shape[num] = shape[i];
+			num++;
 		}
 	}
 
-	for (int i = 0; i < count; i++) {
-		cout << "Area:" << shape[i]->getArea() << endl;
+	shapeArea50 = new Shape*[num];
+	for (int i = 0; i < num; i++) {
+		shapeArea50[i] = shape[i];
+		cout <<"size:"<<shapeArea50[i]->getArea() << endl;
 	}
+	count = num;
+	return shapeArea50;
+}
+
+void shapeDel(Shape** shape, int size)
+{
+	for (int i = 0; i < size; i++) {
+		delete shape[i];
+	}
+}
+
+int main()
+{ 
+
+	Shape* shape[SHAPE_NUM];
+	int count = 0;
+	shapeInit(shape, SHAPE_NUM);
+	Shape** shapeArea50 = selectArea50(shape, SHAPE_NUM, count);
+
+	for (int i = 0; i < count; i++) {
+		cout <<"size:"<<shapeArea50[i]->getArea() << endl;
+	}
+	shapeDel(shape, count);
+	delete[] shapeArea50;
 	return 0;
 }
